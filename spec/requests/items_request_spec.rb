3188e144a970b6ca54817f51f6ffa6471a931132
Dione Wilson <dionew1@gmail.com>
2018-01-02 09:38:45 -0700

    Index test for API passing

--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -4,19 +4,18 @@ describe 'user can view a list of items' do
   it "when visiting '/api/v1/items'" do
     create_list(:item, 3)
 
-    visit "/api/v1/items"
+    get "/api/v1/items"
 
     items = JSON.parse(response.body)
-    byebug
+    item = items.first
+
     expect(response).to be_success
-    expect(items.firt).to have_key "id"
-    expect(items.firt).to have_key "name"
-    expect(items.firt).to have_key "description"
-    expect(items.firt).to have_key "image_url"
-    expect(items.firt).to_not have_key "created_at"
-    expect(items.firt).to_not have_key "updated_at"
+    expect(item).to have_key "id"
+    expect(item).to have_key "name"
+    expect(item).to have_key "description"
+    expect(item).to have_key "image_url"
+    expect(item).to_not have_key "created_at"
+    expect(item).to_not have_key "updated_at"
 
   end
 end
-
- # id, name, description, and image_url but not the created_at or updated_at
