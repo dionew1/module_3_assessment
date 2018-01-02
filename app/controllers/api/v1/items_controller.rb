0acb215f19cdabe3402d11be8647c0d3a19e1773
Dione Wilson <dionew1@gmail.com>
2018-01-02 09:54:19 -0700

    API for item is complete with testing via postman for non-get routes.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,4 +8,20 @@ class Api::V1::ItemsController < ActionController::API
     render json: Item.find(params[:id])
   end
 
+  def create
+    item = Item.create(item_params)
+    redirect_to api_v1_item_path(item)
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item != nil
+      item.destroy
+    end
+  end
+
+  private
+    def item_params
+      params.permit(:name, :description, :image_url)
+    end
 end
