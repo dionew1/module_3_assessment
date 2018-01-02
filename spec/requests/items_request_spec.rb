d4012935c28d41d9bea8cee242a61b5f9dd03169
Dione Wilson <dionew1@gmail.com>
2018-01-02 09:42:12 -0700

    Show api test passing

--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -16,6 +16,23 @@ describe 'user can view a list of items' do
     expect(item).to have_key "image_url"
     expect(item).to_not have_key "created_at"
     expect(item).to_not have_key "updated_at"
+  end
+end
 
+describe 'user can visit single item' do
+  it "when visiting '/api/v1/item/1'" do
+    create_list(:item, 3)
+
+    get "/api/v1/items/1"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item).to have_key "id"
+    expect(item).to have_key "name"
+    expect(item).to have_key "description"
+    expect(item).to have_key "image_url"
+    expect(item).to_not have_key "created_at"
+    expect(item).to_not have_key "updated_at"
   end
 end
